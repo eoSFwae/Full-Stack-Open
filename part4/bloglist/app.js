aee49f200787bac67352ccf3d9ff918994854af2
Paul <54538759+eoSFwae@users.noreply.github.com.>
2025-07-26 21:17:28 -0400

    exercise done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,16 +2,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require("./utils/config")
+const logger = require('./utils/logger')
 const app = express()
 const mongoUrl = config.MONGODB_URI
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 // App is responsible for database connection
-mongoose.connect(mongoUrl).then(() => console.log('Connected to MongoDB')).catch(e => console.error(e))
+mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+        .catch((error)=>{
+            logger.error('error connection to MongoDB:', error.message)
+        })
+
 
 app.use(express.json())
+app.use(middleware.requestLogger)
 app.use("/api/blogs", blogsRouter)
-
+app.use("/api/users", usersRouter)
+app.use('/api/login', loginRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
